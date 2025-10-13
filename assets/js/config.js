572012a7768b423797ed283a2eb823ff2f6fd670
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2025-10-13 12:18:40 +0700

    Create config.js
    add config

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+// PUBLIC (aman untuk browser)
+export const SUPABASE_URL = 'https://wzyhcmwnyygxoprxtnqx.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6eWhjbXdueXlneG9wcnh0bnF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxNzY1MjQsImV4cCI6MjA3NTc1MjUyNH0.TlHsmg9-i4j9HIBNDtOSSPSFEm4du4ewcfd-AVEB6jM';
